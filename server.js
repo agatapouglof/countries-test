f56ecc18e3c5fc7cd329d626f439f3a8ac10f48a
ndjol <daniel.nougbele@objectif-libre.ci>
2018-12-05 13:47:10 +0000

    heroku : correct  serve directory  deploy

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 //Install express server
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/operantic-test'));
 
-// Start the app by listening on the default Heroku port
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname,'/dist/operantic-test/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
